79f5ae34598defd94328d136f90e69bac1568827
proHyundo <128882585+proHyundo@users.noreply.github.com>
2023-11-15 11:34:04 +0900

    Title: 오목, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 231115/오목/O-mok.py
+++ 231115/오목/O-mok.py
@@ -13,45 +13,48 @@ def in_range(i, j):
     else:
         return False
 
-def check_omock(i, j, target):
+def check_omock(i, j, target, direction):
     global cnt    
-    
     cnt += 1
     checked[i][j] = True
     if cnt == 5:
         return i, j
 
-    for dx, dy in zip(dxs, dys): 
-        nx = dx + i
-        ny = dy + j
-        if in_range(nx, ny) and target == grid[nx][ny] and not checked[nx][ny]:
-            return check_omock(nx, ny, target)
+    dx, dy = direction
+    nx = dx + i
+    ny = dy + j
+    if in_range(nx, ny) and target == grid[nx][ny] and not checked[nx][ny]:
+        return check_omock(nx, ny, target, direction)
     return False
 
-
+exit_flag = False
 for i in range(19):
-    exit_flag = False
     for j in range(19):
         if grid[i][j] == 1 or grid[i][j] == 2:
-            cnt = 0
-            result = check_omock(i, j, grid[i][j])
-            if result:
-                ni, nj = result
-                ans_x = 0
-                ans_y = 0
-                if i == ni:
-                    ans_x = i+1
-                else:
-                    ans_x = int((i + 1 + ni + 1) / 2)
+            for direction in zip(dxs, dys):
+                cnt = 0
+                result = check_omock(i, j, grid[i][j], direction)
+                if result:
+                    ni, nj = result
+                    ans_x = 0
+                    ans_y = 0
 
-                if j == nj:
-                    ans_y = j+1
-                else:
-                    ans_y = int((j + 1 + nj + 1) / 2)
-                
-                print(grid[i][j])
-                print(ans_x, ans_y)
-                exit_flag = True
-                break
+                    if i == ni:
+                        ans_x = i+1
+                    else:
+                        ans_x = int((i + 1 + ni + 1) / 2)
+
+                    if j == nj:
+                        ans_y = j+1
+                    else:
+                        ans_y = int((j + 1 + nj + 1) / 2)
+                    
+                    print(grid[i][j])
+                    print(ans_x, ans_y)
+                    exit_flag = True
+                    break
     if exit_flag:
         break
+
+if exit_flag == False:
+    print(0)
