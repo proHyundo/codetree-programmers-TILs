74c8c59d83ad627d69cabbffd1c6c32b08e493c7
proHyundo <128882585+proHyundo@users.noreply.github.com>
2023-11-17 09:54:13 +0900

    Title: 구간 중 최대 합, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 231117/구간 중 최대 합/max-sum-of-subarray.py
+++ 231117/구간 중 최대 합/max-sum-of-subarray.py
@@ -4,6 +4,6 @@ arr = list(map(int, input().split()))
 max_val = 0
 
 for i in range(n-k+2):
-    max_val = max(max_val, sum(arr[i:i+3:]))
+    max_val = max(max_val, sum(arr[i:i+k:]))
 
 print(max_val)
