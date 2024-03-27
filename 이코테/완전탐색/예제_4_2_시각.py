97064dd51a4498ad27b993de7ef837e832a80950
proHyundo <guseh08@knou.ac.kr>
2024-03-27 09:07:06 +0900

    Update: 이코테 완전탐색 시간 추가 풀이법

--- 이코테/완전탐색/예제_4_2_시각.py
+++ 이코테/완전탐색/예제_4_2_시각.py
@@ -18,3 +18,34 @@ for h in range(0, n+1):
 
 
 print(ans)
+
+
+def count_times_with_three(N):
+    # 0 ~ N 시간 중 '3' 이 들어간 시간의 개수
+    hours_with_three = sum(1 for hour in range(N + 1) if '3' in str(hour))
+    print(hours_with_three)
+
+    # 3을 포함하는 분 또는 초 개수 : 03, 13, 23, 30-39, 43, 53
+    minutes_with_three = 15
+    seconds_with_three = 15
+
+    # 전체 시 : 0부터 세니까 1 더함
+    total_hours = N + 1
+
+    # '3'이 들어간 시간은 전체 다 카운트
+    total_with_hour_three = hours_with_three * 60 * 60
+
+    # '3'이 들어 가지 않은 시간 = 전체 시 - '3'이 들어간 시
+    hours_without_three = total_hours - hours_with_three
+
+    # '3'이 들어가지 않은 시간은 일부만 카운트
+    total_with_minute_three = hours_without_three * minutes_with_three * 60
+    minutes_without_three = 60 - minutes_with_three
+    total_with_second_three = hours_without_three * minutes_without_three * seconds_with_three
+
+    # Total count
+    total = total_with_hour_three + total_with_minute_three + total_with_second_three
+
+    return total
+
+print(count_times_with_three(n))
