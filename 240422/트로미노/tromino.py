617e0bec3089110c6d85de84bf44b4c18918d9dc
proHyundo <128882585+proHyundo@users.noreply.github.com>
2024-04-22 11:31:24 +0900

    Title: 트로미노, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240422/트로미노/tromino.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
+
+answer = 0
+
+for i in range(n-1):
+    for j in range(m-1):
+        area = [board[i][j], board[i][j+1], board[i+1][j], board[i+1][j+1]]
+        area.sort()
+        answer = max(answer, sum(area[1:]))
+
+
+for i in range(n-2):
+    for j in range(m):
+        answer = max(answer, board[i][j] + board[i+1][j] + board[i+2][j])
+
+
+print(answer)
