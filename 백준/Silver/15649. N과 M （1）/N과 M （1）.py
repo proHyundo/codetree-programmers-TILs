83beb2b1db8d3738b5e74ec83c96ae07af485961
proHyundo <128882585+proHyundo@users.noreply.github.com>
2024-04-30 09:10:44 +0900

    [Silver III] Title: N과 M (1), Time: 148 ms, Memory: 36580 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,19 +1,6 @@
+from itertools import permutations
 N, M = map(int, input().split())
-ans = []
-visited = [False] * (N+1)
-
-
-def dfs(n, lst):
-    if n == M:
-        ans.append(lst)
-        return
-    for num in range(1, N+1):
-        if not visited[num]:
-            visited[num] = True
-            dfs(n+1, lst+[num])
-            visited[num] = False
-
-
-dfs(0, [])
+nums = [x for x in range(1, N+1)]
+ans = list(permutations(nums, M))
 for ele in ans:
     print(*ele)
