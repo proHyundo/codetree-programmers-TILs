67e6d9d96cf23da90a1ccf194cc51c6fbd46369b
proHyundo <guseh08@knou.ac.kr>
2024-05-01 14:21:39 +0900

    add: 다시풀어볼문항 기록할 문서 생성

---
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/1208_flatten.py
@@ -0,0 +1,25 @@
+T = int(input())
+for test_case in range(1, T+1):
+    dump_cnt = int(input())
+    boxes = list(map(int, input().split()))
+    boxes.sort()
+
+    for _ in range(dump_cnt):
+        if boxes[0] == boxes[99] or boxes[99] - boxes[0] == 1:
+            break
+
+        min_index = 0
+        max_index = 99
+        boxes[0] += 1
+        boxes[99] -= 1
+
+        # 앞 부터 부분 정렬
+        while boxes[min_index] > boxes[min_index+1]:
+            boxes[min_index], boxes[min_index+1] = boxes[min_index+1], boxes[min_index]
+            min_index += 1
+        # 뒤 부터 부분 정렬
+        while boxes[max_index] < boxes[max_index-1]:
+            boxes[max_index], boxes[max_index - 1] = boxes[max_index - 1], boxes[max_index]
+            max_index -= 1
+
+    print(f'#{test_case}',boxes[99] - boxes[0])
