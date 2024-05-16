38aa135a3b5d385a2fd86590c43dfe85d67c8785
proHyundo <128882585+proHyundo@users.noreply.github.com>
2024-05-16 12:24:20 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 147 ms, Memory: 49,348 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -5,7 +5,7 @@ for test_case in range(1, T + 1):
     N = int(s_cnt)  # 교환 횟수
     length = len(nums)
     ans = 0
-    visited = []
+    visited = dict()
 
 
     def dfs(n):
@@ -19,10 +19,10 @@ for test_case in range(1, T + 1):
             for j in range(i+1, length):
                 nums[i], nums[j] = nums[j], nums[i]
 
-                chk = int(''.join(nums))
-                if (n, chk) not in visited:
+                chk = int(''.join(nums)) * 10 + n
+                if chk not in visited:
                     dfs(n+1)
-                    visited.append((n, chk))
+                    visited[chk] = True
 
                 nums[i], nums[j] = nums[j], nums[i]
 
