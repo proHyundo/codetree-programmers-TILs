003ee6d9f8a4fae1dc508fc2218169794685fff8
proHyundo <guseh08@knou.ac.kr>
2024-05-16 12:46:09 +0900

    update: swea 1224 add comments

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -5,7 +5,7 @@ for test_case in range(1, T + 1):
     N = int(s_cnt)  # 교환 횟수
     length = len(nums)
     ans = 0
-    visited = dict()
+    visited = dict()  # 해당 문제의 경우 visited를 dictionary 구조로 활용해도 됨.
 
 
     def dfs(n):
@@ -19,7 +19,7 @@ for test_case in range(1, T + 1):
             for j in range(i+1, length):
                 nums[i], nums[j] = nums[j], nums[i]
 
-                chk = int(''.join(nums)) * 10 + n
+                chk = int(''.join(nums)) * 10 + n  # (int(''.join(nums)), n) 과 같은 tuple 형식보다, 해당 방법이 유니크함을 보장하면서 조금 더 빠름
                 if chk not in visited:
                     dfs(n+1)
                     visited[chk] = True
