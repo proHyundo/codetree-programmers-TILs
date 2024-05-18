8edfcc182ff36a194a986d7c8e44be3320b7baa7
proHyundo <128882585+proHyundo@users.noreply.github.com>
2024-05-18 15:04:15 +0900

    [D3] Title: 부분 수열의 합, Time: 1,088 ms, Memory: 65,184 KB -BaekjoonHub

---
+++ SWEA/D3/2817. 부분 수열의 합/README.md
@@ -0,0 +1,15 @@
+# [D3] 부분 수열의 합 - 2817 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV7IzvG6EksDFAXB) 
+
+### 성능 요약
+
+메모리: 65,184 KB, 시간: 1,088 ms, 코드길이: 367 Bytes
+
+### 제출 일자
+
+2024-05-18 15:04
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
@@ -0,0 +1,20 @@
+T = int(input())
+for test_case in range(1, T+1):
+    N, K = map(int, input().split())
+    arr = list(map(int, input().split()))
+    ans = 0
+
+
+    def dfs(n, sm):
+        global ans
+        if n == N:
+            if sm == K:
+                ans += 1
+            return
+
+        dfs(n+1, sm + arr[n])
+        dfs(n+1, sm)
+
+
+    dfs(0, 0)
+    print(f'#{test_case}', ans)
