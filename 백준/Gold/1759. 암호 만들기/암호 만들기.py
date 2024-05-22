d23360ad202dd2e23ef036c407240629778667ce
proHyundo <guseh08@knou.ac.kr>
2024-05-22 10:18:10 +0900

    update: baekjoon 1759 add comment

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,15 +1,13 @@
 L, C = map(int, input().split())
-# 모음 > 1, 자음 > 2
 vowels = {
     "a": True, "e": True, "i": True, "o": True, "u": True
 }
 words = sorted(list(input().split()))
 ans = []
 
-
+# multi tree
 def dfs(n, start, lst):
     if n == L:
-        # 정답 처리
         vowel = not_vowel = 0
         for a in lst:
             if a in vowels:
@@ -24,6 +22,33 @@ def dfs(n, start, lst):
         dfs(n+1, s+1, lst + [words[s]])
 
 
-dfs(0, 0, [])
+# binary tree
+def dfs_binary_tree(n, cnt, lst):
+    # 가지치기
+    if cnt > L:
+        return
+    # 종료조건
+    if n == C:
+        if cnt == L:
+            vowel = not_vowel = 0
+            for a in lst:
+                if a in vowels:
+                    vowel += 1
+                else:
+                    not_vowel += 1
+            if vowel > 0 and not_vowel > 1:
+                ans.append(lst)
+        return
+
+    dfs_binary_tree(n+1, cnt+1, lst + [words[n]])
+    dfs_binary_tree(n+1, cnt, lst)
+
+
+dfs_binary_tree(0, 0, [])
 for ele in ans:
     print(*ele, sep='')
+
+# 해설 영상 팁
+# 1) 문자를 출력할 것이니, lst 대신 문자 타입으로 받는다.
+# 2) 모음의 개수를 매개변수로 받는다.
+# 3) 자음 개수 = L - 모음 개수
