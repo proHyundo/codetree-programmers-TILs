a7cd8f336877ef8cc0c547229a2e3a606d4f376e
proHyundo <128882585+proHyundo@users.noreply.github.com>
2024-05-23 09:33:23 +0900

    [Bronze II] Title: 피보나치 수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.py
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -1,10 +1,9 @@
 N = int(input())
-dp = [0] * (45+1)
+dp = [0] * (N+1)
 dp[1] = 1
-dp[2] = 1
 
-if N > 2:
-    for i in range(3, N+1):
+if N > 1:
+    for i in range(2, N+1):
         dp[i] = dp[i-1] + dp[i-2]
 
 print(dp[N])
