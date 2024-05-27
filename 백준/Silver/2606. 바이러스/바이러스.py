05057fd408ff759913e70954ce02df48f6da84c8
proHyundo <guseh08@knou.ac.kr>
2024-05-27 10:02:33 +0900

    update: baekjoon 2606 add dfs solution

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -4,7 +4,7 @@ M = int(input())  # 네트워크 수
 adj = [
     [] for _ in range(N+1)
 ]
-ans = [False] * (N+1)
+visited = [False] * (N+1)
 
 for _ in range(M):
     n1, n2 = map(int, input().split())
@@ -15,7 +15,7 @@ for _ in range(M):
 def bfs():
     q = deque()
     q.append(1)
-    ans[1] = True
+    visited[1] = True
     cnt = 0
 
     while q:
@@ -23,9 +23,21 @@ def bfs():
         cnt += 1
 
         for nx in adj[cur]:
-            if not ans[nx]:
+            if not visited[nx]:
                 q.append(nx)
-                ans[nx] = True
+                visited[nx] = True
     return cnt - 1
 
+
+cnt = 0
+def dfs(c):
+    global cnt
+    cnt += 1
+    visited[c] = 1
+    for n in adj[c]:
+        if not visited[n]:
+            dfs(n)
+
+
+print(dfs(1) - 1)
 print(bfs())
