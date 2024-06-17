803fa35b022cfbde9105cfd5849594e7aeac6833
proHyundo <128882585+proHyundo@users.noreply.github.com>
2024-06-17 10:03:10 +0900

    [level 2] Title: 구명보트, Time: 9.11 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,28 +1,20 @@
 def solution(people, limit):
     sort_p = sorted(people)
+    answer = 0
     
     front = 0
     tail = len(people) - 1
     
-    answer = 0
-
-    while True:
-        sm = 0
-
-        if front > tail:
-            break
+    while front <= tail:
         
         if front == tail:
             answer += 1
             break
-        
-        sm += sort_p[tail]
-        tail -= 1
-        
-        if sm + sort_p[front] <= limit:
+
+        if sort_p[tail] + sort_p[front] <= limit:
             front += 1
         
+        tail -= 1
         answer += 1
         
-    
     return answer
