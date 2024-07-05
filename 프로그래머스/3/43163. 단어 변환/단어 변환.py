5044525b8d42a7985328a686755183409824a93c
proHyundo <guseh08@knou.ac.kr>
2024-07-05 13:16:34 +0900

    update: 프로그래머스 43163 오답코드 추가

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -34,3 +34,47 @@ def solution(begin, target, words):
     dfs(0, begin)
 
     return answer if answer != sys.maxsize else 0
+
+# 왜 아래 풀이는 안되는걸까?
+"""
+import sys
+
+
+def solution(begin, target, words):
+    answer = sys.maxsize
+    length = len(words)
+    v = [False] * length
+    if target not in words:
+        return 0
+
+    def check_diff(r, w):
+        cnt = 0
+        for e1, e2 in zip(list(r), list(w)):
+            if e1 != e2:
+                cnt += 1
+            if cnt > 1:
+                break
+        return True if cnt == 1 else False
+
+    def dfs(n, cnt, rst):
+        nonlocal answer
+        if cnt > answer:
+            return
+        if n == length or rst == target:
+            if rst == target:
+                answer = min(answer, cnt)
+            return
+
+        for i in range(length):
+            if not v[i] and check_diff(rst, words[i]) == 1:
+                v[i] = True
+                dfs(n+1, cnt + 1, words[i])
+                v[i] = False
+
+        dfs(n+1, cnt, rst)
+
+
+    dfs(0, 0, begin)
+
+    return answer if answer != sys.maxsize else 0
+"""
