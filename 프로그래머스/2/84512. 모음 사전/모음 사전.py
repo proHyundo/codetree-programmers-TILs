e226a737e48afd71e2fc434b32026c1b3510d93c
proHyundo <guseh08@knou.ac.kr>
2024-07-18 12:34:25 +0900

    update: 프로그래머스 84512 enumerate 사용

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -2,18 +2,16 @@ from itertools import product
 
 def solution(word):
     _list = []
-    count = 0
     answer = 0
     for i in range(1, 6):
         for t in product(['A', 'E', 'I', 'O', 'U'], repeat = i):
             _list.append(''.join(t))
 
     _list.sort()
-    
-    for t in _list:
-        count += 1
+
+    for index, t in enumerate(_list):
         if word == t:
-            answer = count
+            answer = index + 1
             break
 
     return answer
