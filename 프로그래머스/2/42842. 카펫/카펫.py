62e021986c3d96f714674db79c764bd5d7cc3d20
proHyundo <128882585+proHyundo@users.noreply.github.com>
2024-07-23 12:54:58 +0900

    [level 2] Title: 카펫, Time: 0.16 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,10 +1,7 @@
 def solution(brown, yellow):
-    tup = tuple()
     by = brown + yellow
+    # 가로의 길이를 1부터 by까지 순회
     for i in range(1, by+1):
+        # 가로세로가 정수이면서, 가로의 길이가 더 길면서, 노란색의 개수가 가로-2 * 세로-2 조건을 만족할때
         if (by % i == 0) and (i >= by // i) and (yellow == (i-2)*(by//i-2)):
-            tup = ((i,by//i))
-            break
-    
-    
-    return [tup[0], tup[1]]
+            return [i, by//i]
