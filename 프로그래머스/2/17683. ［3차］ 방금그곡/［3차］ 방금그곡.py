ec6424fd81a73a675d866075424643998fe60c31
proHyundo <guseh08@knou.ac.kr>
2024-07-25 14:55:41 +0900

    update: 잘못된 풀이도 기록용으로 추가

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
@@ -38,3 +38,33 @@ def solution(m, musicinfos):
 
     # print('answer', answer)
     return sorted(answer, key=lambda x: (-x[1], x[2]))[0][0] if len(answer) > 0 else "(None)"
+
+# 테케 62.5 통과했던 코드
+# def solution(m, musicinfos):
+#     answer = []
+#     for index, ele in enumerate(musicinfos):
+#         start, end, title, music = ele.split(',')
+#         si = int(str(start)[:2]) * 60 + int(str(start[3:]))
+#         ei = int(str(end)[:2]) * 60 + int(str(end[3:]))
+#
+#         minutes = ''
+#         ml = len(music)
+#         for i in range(ei - si+1):
+#             minutes += music[i % ml]
+#         shap_cnt = minutes.count('#')
+#         for i in range(shap_cnt):
+#             minutes += music[i % ml]
+#         # print(minutes)
+#         # 찾으려는 m 이 minutes 에 존재하며, minutes 에서 m 다음 문자가 '#'이 아니어야 함.
+#         s = 0
+#         while minutes.find(m, s) >= 0:
+#         # if m in minutes:
+#             m_index_in_minutes = minutes.find(m, s)
+#             target_index = m_index_in_minutes + len(m)
+#             # print(m_index_in_minutes, target_index)
+#             if target_index < len(minutes) and minutes[target_index] != '#':
+#                 answer.append((title, ei - si, index))
+#             s = target_index+1
+#
+#     # print(answer)
+#     return sorted(answer, key=lambda x: (-x[1], x[2]))[0][0] if len(answer) > 0 else "(None)"
