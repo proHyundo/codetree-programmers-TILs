dbb608a20d9e252f3e681a7dc7bedc384ee2d152
proHyundo <128882585+proHyundo@users.noreply.github.com>
2024-08-06 14:15:18 +0900

    [Gold III] Title: 앱, Time: 588 ms, Memory: 48056 KB -BaekjoonHub

---
+++ 백준/Gold/7579. 앱/앱.py
@@ -0,0 +1,22 @@
+N, M = map(int, input().split()) # N개의 앱 활성화
+memories = list(map(int, input().split()))
+costs = list(map(int, input().split()))
+max_cost = sum(costs)+1
+dp = [
+    [0] * max_cost for _ in range(N+1)
+]
+answer = max_cost
+
+for i in range(1, N+1):
+    m, c = memories[i-1], costs[i-1]
+    for j in range(0, max_cost):
+        if j < c:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-c] + m)
+
+        if dp[i][j] >= M:
+            answer = min(answer, j)
+
+
+print(answer if M != 0 else 0)
