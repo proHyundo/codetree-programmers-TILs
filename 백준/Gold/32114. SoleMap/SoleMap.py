b491c0b5c4bb3af6ae244faacb76e049f1a146c1
proHyundo <128882585+proHyundo@users.noreply.github.com>
2024-08-14 15:59:48 +0900

    [Gold IV] Title: SoleMap, Time: 528 ms, Memory: 202212 KB -BaekjoonHub

---
+++ 백준/Gold/32114. SoleMap/SoleMap.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+N, M = map(int, input().split()) # 도시의 수, 차량 정보
+
+# 유동 차량 초기 세팅, 도로 세팅
+cars = [0] * (N+1)
+loads = [0] + list(map(int, input().split()))
+
+# 유동 차량 세팅
+for _ in range(M):
+    u, v, x = map(int, input().split())
+    cars[u] += x
+    cars[v] -= x  # Adjusting for end point
+
+# Process the difference array to get the actual car loads
+for i in range(1, N+1):
+    cars[i] += cars[i-1]
+
+
+def find_min_cost(l, c):
+    common = c // l # 공통
+    diff = c - (c // l * l) # 차이나는 값
+    return ((common+1) ** 2 * diff) + (common ** 2 * (l-diff))
+
+
+for i in range(1, N):
+    load, car = loads[i], cars[i]
+    if load == 1:
+        sys.stdout.write(str(car**2) + '\n')
+    elif load >= car:
+        sys.stdout.write(str(car) + '\n')
+    else:
+        sys.stdout.write(str(find_min_cost(load, car)) + '\n')
