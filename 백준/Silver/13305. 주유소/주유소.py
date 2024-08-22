13296edf0657ec56e3294d0b3c3e9676ef1572c6
proHyundo <128882585+proHyundo@users.noreply.github.com>
2024-08-22 14:07:56 +0900

    [Silver III] Title: 주유소, Time: 240 ms, Memory: 31120 KB, Score: 41 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+DP = [0] + [sys.maxsize] * N
+
+load_lst = [0] + list(map(int, input().split()))     # 2 3 1
+cost_lst = [0] + list(map(int, input().split()))    # 5 2 4 1
+DP[2] = cost_lst[1] * load_lst[1]
+
+for i in range(1, N): # i = 1,2,3번 도시에서 주유
+    for j in range(i+1, N+1): # j = 2,3,4번 도시로 이동
+        DP[j] = min(DP[j], DP[j-1] + (cost_lst[i] * load_lst[j-1]))
+    # print(DP)
+
+print(DP[N])
