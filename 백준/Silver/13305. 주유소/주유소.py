75b6da7f000e773a50b4972fe98dc11c83e6f7ac
proHyundo <128882585+proHyundo@users.noreply.github.com>
2024-08-28 17:30:16 +0900

    [Silver III] Title: 주유소, Time: 92 ms, Memory: 47064 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -2,19 +2,15 @@ import sys
 
 input = sys.stdin.readline
 N = int(input())
-
 load_lst = [0] + list(map(int, input().split()))     # 2 3 1
 cost_lst = [0] + list(map(int, input().split()))    # 5 2 4 1
 
-if cost_lst.count(1) == N:
-    print(sum(load_lst))
-else:
-    DP = [0] + [sys.maxsize] * N
-    DP[2] = cost_lst[1] * load_lst[1]
+min_cost = cost_lst[1]
+sm_cost = 0
 
-    for i in range(1, N): # i = 1,2,3번 도시에서 주유
-        for j in range(i+1, N+1): # j = 2,3,4번 도시로 이동
-            DP[j] = min(DP[j], DP[j-1] + (cost_lst[i] * load_lst[j-1]))
-        # print(DP)
+for i in range(1, N):
+    if min_cost > cost_lst[i]:
+        min_cost = cost_lst[i]
+    sm_cost += min_cost * load_lst[i]
 
-    print(DP[N])
+print(sm_cost)
