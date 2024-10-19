b82e739cf9dc90401c84fb1336d1681086e16305
proHyundo <128882585+proHyundo@users.noreply.github.com>
2024-10-19 15:46:54 +0900

    [Gold V] Title: 보물섬, Time: 944 ms, Memory: 120448 KB -BaekjoonHub

---
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -0,0 +1,40 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+N, M = map(int, input().split())
+board = [
+    list(input()) for _ in range(N)
+]
+
+dxs = [0,1,0,-1]
+dys = [1,0,-1,0]
+
+def bfs(sx, sy):
+    q = deque()
+    q.append((sx, sy))
+    visited = [
+        [0] * M for _ in range(N)
+    ]
+    visited[sx][sy] = 1
+    cnt = 0
+
+    while q:
+        cx, cy = q.popleft()
+        for dx, dy in zip(dxs, dys):
+            nx = cx + dx
+            ny = cy + dy
+            if 0 <= nx < N and 0 <= ny < M and board[nx][ny] == 'L' and visited[nx][ny] == 0:  # 방문하지 않았다면 더 탐색
+                visited[nx][ny] = visited[cx][cy] + 1
+                q.append((nx, ny))
+                cnt = max(cnt, visited[nx][ny])
+
+    return cnt - 1
+
+ans = 0
+for i in range(N):
+    for j in range(M):
+        if board[i][j] == 'L':
+            ans = max(ans, bfs(i, j))
+
+
+print(ans)
