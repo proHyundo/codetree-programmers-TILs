a4ce890fcb21e14a1a545f19fb346bb297a92599
proHyundo <guseh08@knou.ac.kr>
2024-10-30 20:52:38 +0900

    update: 백준 24337 건물의 개수가 많은 경우를 생각하지 못했음

--- 백준/24337.py
+++ 백준/24337.py
@@ -1,1 +1,12 @@
 N, a, b = map(int, input().split()) # 건물 개수, 가희가 볼 수 있는 건물 개수, 단비가 볼 수 있는 건물 개수
+front = [
+    h for h in range(1, a+1)
+]
+back = []
+if b > 1:
+    back = [
+        h for h in range(1, b)
+    ]
+    front += back[::-1]
+
+print(*front)
